ebfd154e3a2fd3b6b3845830cd7f5ad70779e1c9
Artemid1yy <dostan557@gmail.com>
2026-09-16 14:11:51 +0300

    Добавил lab02 в Code overview, images и src

--- src/lab01/06*.py
+++ src/lab01/06*.py
@@ -1,8 +1,20 @@
-n = int(input("Сколько людей пришло: "))
+# n = int(input("Сколько людей пришло: "))
+# full_time = 0
+# part_time = 0
+# for i in range(n):
+#     surname, name, age, form = input('in_1: ').split()
+#     if form == "True":
+#         full_time += 1
+#     else:
+#         part_time += 1
+# print(full_time, part_time)
+
+
+n = int(input("in_1: "))
 full_time = 0
 part_time = 0
 for i in range(n):
-    surname, name, age, form = input('in_1: ').split()
+    surname, name, age, form = input(f'in_{i+2}: ').split()
     if form == "True":
         full_time += 1
     else:
