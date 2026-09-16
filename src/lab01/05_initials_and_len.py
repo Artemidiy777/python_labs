afc7ee9fde8fc4b82fdea8b9cc075826b0ca9a5e
Artemid1yy <dostan557@gmail.com>
2026-09-16 13:52:34 +0300

    Добавил lab02 в Code overview, images и src

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -1,10 +1,10 @@
-name = input('Ваше ФИО: ')
-name = name.split()
+name_input = input('Ваше ФИО: ')
+name = name_input.split()
 
 if len(name) != 3:
     print('Нужно ввести три слова: фамилию, имя и отчество')
 else:
     initials = f'{name[0][0]}{name[1][0]}{name[2][0]}.'
-    total_length = len(name[0]) + len(name[1]) + len(name[2])
+    total_length = len(name_input)
     print(f'''Инициалы: {initials}
 Длина (символов): {total_length}''')
